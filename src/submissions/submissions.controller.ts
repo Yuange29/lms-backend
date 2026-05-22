cb75b87b665d61cdb172793f6370378c59653035
Yuange29 <tancat7895@gmail.com>
2026-05-22 20:07:03 +0700

    create submission controller + service + add som e thing in module

--- src/submissions/submissions.controller.ts
+++ src/submissions/submissions.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Roles } from 'src/common/decorators/check-roles.decorator';
+import { GetUser } from 'src/common/decorators/current-user.decorator';
+import { Role } from 'src/common/enums/roles.enum';
+import { JwtAccessGuard } from 'src/common/guards/jwt-access.guard';
+import { RolesGuard } from 'src/common/guards/role-check.guard';
+
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+
+import { SubmissionAnswersDto } from './dto/create-submissions.dto';
+import { SubmissionsService } from './submissions.service';
 
 @Controller('submissions')
-export class SubmissionsController {}
+@UseGuards(JwtAccessGuard)
+export class SubmissionsController {
+  constructor(private readonly submissionService: SubmissionsService) {}
+
+  @Post(':quizId')
+  @UseGuards(RolesGuard)
+  @Roles(Role.student)
+  async createSubmission(
+    @Param('quizId') quizId: string,
+    @GetUser('id') userId: string,
+    @Body() createReq: SubmissionAnswersDto,
+  ) {
+    return await this.submissionService.create(createReq, userId, quizId);
+  }
+
+  @UseGuards(RolesGuard)
+  @Roles(Role.student)
+  @Get(':quizId/my')
+  async getSubmission(
+    @Param('quizId') quizId: string,
+    @GetUser('id') userId: string,
+  ) {
+    return await this.submissionService.getQuiz(quizId, userId);
+  }
+}
+// POST   /quizzes/:quizId/submissions        # student nộp bài
+// GET    /quizzes/:quizId/submissions/my     # student xem bài của mình
+// GET    /quizzes/:quizId/submissions        # instructor xem tất cả
+// GET    /submissions/:id                    # chi tiết bài nộp + submission_answers
